4187815c018b8292985c971e173a46fb71233caa
deus mouse <smulsky.rv@gmail.com>
2021-08-28 13:12:13 +0300

    добавил список юзеров

--- bot_ffmpeg.py
+++ bot_ffmpeg.py
@@ -1,4 +1,5 @@
 # - *- coding: utf- 8 - *-
+import json
 
 import telebot
 import speech_recognition as sr
@@ -15,31 +16,45 @@ r = sr.Recognizer()
 
 bot = telebot.TeleBot(config.bot_token)
 
-
-@bot.message_handler(content_types=["voice"])
-def voice_handler(message):
-    id_file = message.voice.file_id
-    file = bot.get_file(id_file)
-    down_file = bot.download_file(file.file_path)
-    with open("voice_ogg.ogg", "wb") as f:
-        f.write(down_file)
-
-    process = subprocess.run(['ffmpeg', '-i', 'voice_ogg.ogg', 'voice_wav.wav', '-y'])
-
-    file = sr.AudioFile('voice_wav.wav')
-
-    try:
-        with file:
-            audio = r.record(file)
-            text = r.recognize_google(audio, language="ru_RU")
-
-            bot.send_message(message.chat.id, text, reply_to_message_id=message.message_id)
-    except Exception as ex:
-        bot.send_message(message.chat.id, "пшык/рыг/пердежь")
-
-
-mac_pattern_1 = r'[Мм]ак'
+mac_pattern_1 = r'[Мм]а[кг]'
 mac_pattern_2 = r'[Гг][ао]вно+'
+mac_pattern_3 = r'г\*{4}'
+
+son_pattern_1 = r'[Мм]олодец'
+son_pattern_2 = r'[Сс]ына*'
+
+all_pattern_1 = r'[Рр]ебят'
+all_pattern_2 = r'@all'
+all_pattern_3 = r'@все'
+
+users = []
+
+# @bot.message_handler(content_types=["voice"])
+# def voice_handler(message):
+#     id_file = message.voice.file_id
+#     file = bot.get_file(id_file)
+#     down_file = bot.download_file(file.file_path)
+#     with open("voice_ogg.ogg", "wb") as f:
+#         f.write(down_file)
+#
+#     process = subprocess.run(['ffmpeg', '-i', 'voice_ogg.ogg', 'voice_wav.wav', '-y'])
+#
+#     file = sr.AudioFile('voice_wav.wav')
+#
+    # try:
+    #     with file:
+    #         audio = r.record(file)
+    #         text = r.recognize_google(audio, language="ru_RU")
+    #
+    #         matched_1 = re.search(mac_pattern_1, text)
+    #         matched_3 = re.search(mac_pattern_3, text)
+    #
+    #         bot.send_message(message.chat.id, text, reply_to_message_id=message.message_id)
+    #         if matched_1 and matched_3:
+    #             bot.send_message(message.chat.id, "сам ты говно", reply_to_message_id=message.message_id)
+    #
+    # except Exception as ex:
+    #     bot.send_message(message.chat.id, "пшык/рыг/пердежь", reply_to_message_id=message.message_id)
 
 
 @bot.message_handler(content_types=["text"])
@@ -48,6 +63,18 @@ def text_handler(message):
     matched_2 = re.search(mac_pattern_2, message.text)
     if matched_1 and matched_2:
         bot.send_message(message.chat.id, "сам ты говно", reply_to_message_id=message.message_id)
+
+    matched_4 = re.search(son_pattern_1, message.text)
+    matched_5 = re.search(son_pattern_2, message.text)
+    if matched_4 and matched_5:
+        bot.send_message(message.chat.id, "спасибо создатель \U0001F916", reply_to_message_id=message.message_id)
+
+    # matched_5 = re.search(all_pattern_1, message.text)
+
+    if message.from_user.id not in users:
+        users.append(message.from_user.id)
+        username = bot.get_chat_member(message.chat.id, message.from_user.id).user.username
+        bot.send_message(279478014, f"новый пользователь {username} = {message.from_user.id}")
 
 
 bot.polling(none_stop=True)
