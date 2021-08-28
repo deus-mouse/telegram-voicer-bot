a30f9e8e112a1635e0b3b8c661271624322cd14f
deus mouse <smulsky.rv@gmail.com>
2021-08-28 11:30:10 +0300

    добавил руг на "мак говно"

--- bot_ffmpeg.py
+++ bot_ffmpeg.py
@@ -5,6 +5,8 @@ import speech_recognition as sr
 import subprocess
 from importlib import reload
 import sys
+import re
+
 reload(sys)
 
 import config
@@ -15,7 +17,7 @@ bot = telebot.TeleBot(config.bot_token)
 
 
 @bot.message_handler(content_types=["voice"])
-def handle(message):
+def voice_handler(message):
     id_file = message.voice.file_id
     file = bot.get_file(id_file)
     down_file = bot.download_file(file.file_path)
@@ -34,6 +36,18 @@ def handle(message):
             bot.send_message(message.chat.id, text, reply_to_message_id=message.message_id)
     except Exception as ex:
         bot.send_message(message.chat.id, "пшык/рыг/пердежь")
+
+
+mac_pattern_1 = r'[Мм]ак'
+mac_pattern_2 = r'[Гг][ао]вно+'
+
+
+@bot.message_handler(content_types=["text"])
+def text_handler(message):
+    matched_1 = re.search(mac_pattern_1, message.text)
+    matched_2 = re.search(mac_pattern_2, message.text)
+    if matched_1 and matched_2:
+        bot.send_message(message.chat.id, "сам ты говно", reply_to_message_id=message.message_id)
 
 
 bot.polling(none_stop=True)
