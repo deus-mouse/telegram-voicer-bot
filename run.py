1f6710e540a97031006ebbdcf889ab7808243d8a
deusmouse <transferave@gmail.com>
2022-10-14 10:45:37 +0300

    infinity_polling

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from bot.bot_handler import bot
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    # bot.polling(none_stop=True)
+    bot.infinity_polling(timeout=10, long_polling_timeout=5)
